e9e66ba1731e1928f97a76726378d6ce7cea49cf
faisal-din <128973176+faisal-din@users.noreply.github.com>
2024-09-03 17:52:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -19,20 +19,4 @@ function search() {
   }
 }
 
-// const search = () => {
-//   const searchBox = document.getElementById("search-item").value.toUpperCase();
-//   const storeItems = document.getElementById("product-list");
 
-//   const product = document.querySelectorAll(".product");
-//   const productName = storeItems.getElementsByTagName("h2");
-
-//   for (let i = 0; i < productName.length; i++) {
-//     let match = product[i].getElementsByTagName('h2')[0];
-
-//       if (match.innerHTML.toUpperCase().indexOf(searchBox) > -1) {
-//         product[i].style.display = "block";
-//       } else {
-//         product[i].style.display = "none";
-//       }
-//     }
-//   }
