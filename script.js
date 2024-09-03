51fbbbe750b8c3dcbee2f2ff96b99600e697130b
faisal <engrfd333@gmail.com>
2024-09-03 17:44:47 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const searchBox = document.getElementById('search-item');
+
+searchBox.addEventListener('keyup', search);
+function search() {
+  const searchValue = searchBox.value.toUpperCase();
+  const storeItems = document.getElementById('product-list');
+
+  const product = document.querySelectorAll('.product');
+  const productName = storeItems.getElementsByTagName('h2');
+
+  for (let i = 0; i < productName.length; i++) {
+    let match = product[i].getElementsByTagName('h2')[0];
+
+    if (match.innerHTML.toUpperCase().indexOf(searchValue) > -1) {
+      product[i].style.display = 'block';
+    } else {
+      product[i].style.display = 'none';
+    }
+  }
+}
+
+// const search = () => {
+//   const searchBox = document.getElementById("search-item").value.toUpperCase();
+//   const storeItems = document.getElementById("product-list");
+
+//   const product = document.querySelectorAll(".product");
+//   const productName = storeItems.getElementsByTagName("h2");
+
+//   for (let i = 0; i < productName.length; i++) {
+//     let match = product[i].getElementsByTagName('h2')[0];
+
+//       if (match.innerHTML.toUpperCase().indexOf(searchBox) > -1) {
+//         product[i].style.display = "block";
+//       } else {
+//         product[i].style.display = "none";
+//       }
+//     }
+//   }
